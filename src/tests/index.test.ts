6e6618d0fe365de49ac175740781d2fe6c7b0134
Anshul Parik <anshulparik@gmail.com>
2024-10-07 22:22:18 +0530

    feat: first testcase added, jest config added, new scripts added

---
+++ src/tests/index.test.ts
@@ -0,0 +1,22 @@
+/* ARRANGE, ACT AND ASSERT 
+
+ARRANGE: to setup a test. Like arrage describe, write
+ACT: Actual execution of the fuction
+ASSET: to check the output function
+describe: a way of grouping the test together
+*/
+
+describe("sum 2 numbers", () => {
+  // test1 - Arrange
+  test("should return the addition of 2 integers", () => {
+    // Act
+    const result = sumNumbers(2, 4);
+
+    // Expect
+    expect(result).toBe(6);
+  });
+});
+
+function sumNumbers(a: number, b: number): number {
+  return a + b;
+}
